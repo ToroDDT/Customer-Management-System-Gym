c7c2f4c44c498caab97245afdb5c9e7ea68781cf
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-30 19:32:44 -0800

    feat: record new leads

---
+++ Frontend/src/Dashboard-Components/People/leads-Component.tsx
@@ -0,0 +1,12 @@
+import { AddButton } from "../MyDay-Component";
+
+function LeadsInfo() {
+  return (
+    <div>
+      <div>
+        People <AddButton />
+      </div>
+      <div>New Leads </div>
+    </div>
+  );
+}
