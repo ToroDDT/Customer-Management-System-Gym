d12a52d293bb7e3ca96bc9d9f153581639ed5ace
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-30 21:32:47 -0800

    Fetch leads data from server and then display data

--- Frontend/src/Dashboard-Components/People/leads-Component.tsx
+++ Frontend/src/Dashboard-Components/People/leads-Component.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import { AddButton } from "../MyDay-Component";
 
 function LeadsInfo() {
+  const [arrayOfLeads, setArrayofLeads] = useState([{ name: "david" }]);
+  const url = "Localhost:4000/leads-records";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => setArrayofLeads(response[0]))
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div>
       <div>
         People <AddButton />
       </div>
       <div>New Leads </div>
+      <ul>
+        {arrayOfLeads.map((lead) => (
+          <li>{lead.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export default LeadsInfo;
