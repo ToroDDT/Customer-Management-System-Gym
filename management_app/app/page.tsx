80eb5e777b86e965fc4f9169ef47be65af69b023
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-02 23:17:26 -0800

    Create sidenav component

--- management_app/app/page.tsx
+++ management_app/app/page.tsx
@@ -1,5 +1,20 @@
 import Image from "next/image";
+import Button from "@mui/material/Button";
+import { AddButton } from "./dashboard/MyDay-Component";
 
 export default function Home() {
-  return <main className=""></main>;
+  return <SideNav />;
+}
+
+function SideNav() {
+  return (
+    <div className="bg-blue-500 flex flex-col">
+      <Button variant="contained">Dashboard</Button>
+      <Button variant="contained">People</Button>
+      <Button variant="contained">Communicate</Button>
+      <Button variant="contained">Appointments</Button>
+      <Button variant="contained">Plans</Button>
+      <Button variant="contained">Products</Button>
+    </div>
+  );
 }
