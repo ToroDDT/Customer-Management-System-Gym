a4e7e2d08ee3417ae022394908d34003c2211d83
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-02 20:11:32 -0800

    Remove try and catch blocks
    Removed these due to asyncHandler passing errors and exceptions to our error handler

--- management_app/express/server/controllers/leadsController.js
+++ management_app/express/server/controllers/leadsController.js
@@ -1,24 +1,17 @@
 const asyncHandler = require("express-async-handler");
 const PotentialMember = require("../models/LeadModel");
 
-const newLead = async (req, res, next) => {
-  try {
-    const newLead = new PotentialMember({
-      Email: req.body.Email,
-      FirstName: req.body.First_Name,
-      LastName: req.body.Last_Name,
-      PostalCode: req.body.PostalCode,
-      MobilePhone: req.body.Mobile_Phone,
-      Objectives: req.body.Objectives,
-      RefferedBy: req.body.Reffered_By,
-      Notes: req.body.Notes,
-    });
-    await newLead.save();
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-  next();
-};
-
+const newLead = asyncHandler(async (req, res, next) => {
+  const newLead = new PotentialMember({
+    Email: req.body.Email,
+    FirstName: req.body.First_Name,
+    LastName: req.body.Last_Name,
+    PostalCode: req.body.PostalCode,
+    MobilePhone: req.body.Mobile_Phone,
+    Objectives: req.body.Objectives,
+    RefferedBy: req.body.Reffered_By,
+    Notes: req.body.Notes,
+  });
+  await newLead.save();
+});
 module.exports = { newLead };
