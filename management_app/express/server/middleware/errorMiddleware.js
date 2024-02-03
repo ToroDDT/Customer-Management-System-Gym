d593f0c8fdc4afc121ac34d83b96ba5448806897
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-02 23:17:59 -0800

    Fix: fix spelling

--- management_app/express/server/middleware/errorMiddleware.js
+++ management_app/express/server/middleware/errorMiddleware.js
@@ -9,7 +9,6 @@ const errorHandler = (err, req, res, next) => {
     stack: process.env.NODE_ENV === "development" ? err.stack : {},
   });
 };
-
-errorHandler.module.exports = {
+module.exports = {
   errorHandler,
 };
