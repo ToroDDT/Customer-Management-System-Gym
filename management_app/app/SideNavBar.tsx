81d32ab381ccb5225f84b635de60f529fd26b326
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-22 23:52:32 -0800

    Import react icons

---
+++ management_app/app/SideNavBar.tsx
@@ -0,0 +1,9 @@
+"use client";
+import Button from "@mui/material/Button";
+import { title } from "process";
+import { useState } from "react";
+import { BsArrowLeftShort } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
+import { BsChevronDown } from "react-icons/bs";
+import Link from "next/link";
+import { v4 as uuidv4 } from "uuid";
