ddf95bfd9ccbd01916ec6875b9c43e48ee6caf78
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-22 23:56:58 -0800

    Add menu options to side nav bar

--- management_app/app/SideNavBar.tsx
+++ management_app/app/SideNavBar.tsx
@@ -26,3 +26,89 @@ export function SideNav() {
     </div>
   );
 }
+
+function MenuOptions({ open }) {
+  const [subMenuOpen, SetSubMenuOpen] = useState(false);
+  const [peopleMenu, setPeopleMenu] = useState(false);
+  const [communicationMenus, setCommnunicationMeunus] = useState(false);
+  const [appointmentsMenu, setAppointementsMenu] = useState(false);
+  const [paymentMenu, setPaymentMenu] = useState(false);
+  const [plansMenu, setPlansMenu] = useState(false);
+  const [reportsMenu, setReportsMenu] = useState(false);
+  const menus = [
+    {
+      title: "People",
+      subMenu: true,
+      setToggle: () => {
+        setPeopleMenu(!peopleMenu);
+      },
+      isToggle: peopleMenu,
+      index: uuidv4(),
+      subMenuOptions: [
+        { subMenuTitle: "Members", href: "/members/index" },
+        { subMenuTitle: "Leads", href: "/members/leads" },
+        { subMenuTitle: "users", href: "/users/index" },
+      ],
+    },
+    {
+      title: "Communicate",
+      subMenu: true,
+      setToggle: () => {
+        setCommnunicationMeunus(!communicationMenus);
+      },
+      isToggle: communicationMenus,
+      index: uuidv4,
+      subMenuOptions: [
+        { subMenuTitle: "Automatic", href: "/communications/automatic" },
+        { subMenuTitle: "Email", href: "/communications/email" },
+        { subMenuTitle: "Message", href: "/communications/email" },
+      ],
+    },
+    {
+      title: "Appointments",
+      subMenu: true,
+      setToggle: () => {
+        setAppointementsMenu(!appointmentsMenu);
+      },
+      isToggle: appointmentsMenu,
+      index: uuidv4(),
+      subMenuOptions: [{ subMenuTitle: "Appointments", href: "/appointments" }],
+    },
+    {
+      title: "Plans",
+      subMenu: true,
+      setToggle: () => {
+        setPlansMenu(!plansMenu);
+      },
+      isToggle: plansMenu,
+      index: uuidv4(),
+      subMenuOptions: [
+        { subMenuTitle: "Plans", href: "/plans/index" },
+        { subMenuTitle: "Plan Categories", href: "/plans/categories" },
+        { subMenuTitle: "Discounts", href: "plans/discounts" },
+      ],
+    },
+    {
+      title: "Payments",
+      subMenu: true,
+      setToggle: () => {
+        setPaymentMenu(!paymentMenu);
+      },
+      isToggle: paymentMenu,
+      index: uuidv4(),
+      subMenuOptions: [{ subMenuTitle: "Invoices", href: "/invoices", key: 5 }],
+    },
+    {
+      title: "Reports",
+      subMenu: true,
+      setToggle: () => {
+        setReportsMenu(!reportsMenu);
+      },
+      isToggle: reportsMenu,
+      index: uuidv4(),
+      subMenuOptions: [
+        { subMenuTitle: "Metrics", href: "/reports/metrics", key: 6 },
+      ],
+    },
+  ];
+}
