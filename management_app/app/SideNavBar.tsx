7b7052ca8141b8bc3e7689d6bbd6b0832db2c72d
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-22 23:53:51 -0800

    Create side nav bar

--- management_app/app/SideNavBar.tsx
+++ management_app/app/SideNavBar.tsx
@@ -7,3 +7,22 @@ import { BsSearch } from "react-icons/bs";
 import { BsChevronDown } from "react-icons/bs";
 import Link from "next/link";
 import { v4 as uuidv4 } from "uuid";
+
+export function SideNav() {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <div
+      className={`h-screen p-5 pt-8 bg-blue-600 ${
+        open ? "w-72" : "w-20"
+      } duration-300 relative`}
+    >
+      <BsArrowLeftShort
+        className={`text-3xl bg-white rounded-full absolute -right-3 top-9 border cursor-pointer`}
+        onClick={() => setOpen(!open)}
+      />
+      <SearchBar open={open} />
+      <MenuOptions open={open} />
+    </div>
+  );
+}
