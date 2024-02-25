5c1d2565f4a5ea9950ac81c87054fed5218b9c5b
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-24 18:17:34 -0800

    Create element to display google calendar info

--- management_app/app/dashboard/events.tsx
+++ management_app/app/dashboard/events.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import Button from "@mui/material/Button";
 import Calendar from "./date";
 
 export default function MyDay() {
   return (
-    <div>
-      <div className="bg-blue-600 text-white p-5">My Day</div>
+    <div className="bg-white rounded-md mx-5 mb-5">
+      <div className="bg-blue-600 text-white p-5 rounded-md">My Day</div>
       <Calendar />
       <GoogleCalendar />
     </div>
@@ -12,5 +13,18 @@ export default function MyDay() {
 }
 
 function GoogleCalendar() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col justify-center">
+      <div className="mb-2 mx-10 text-zinc-400 flex justify-center pt-10 pb-15 mt-5">
+        Connect your Google Calendar account to get a complete snapshot of your
+        day.
+      </div>
+      <Button
+        className=" mx-20 mt-5 text-white bg-slate-300 mb-10"
+        variant="contained"
+      >
+        Connect to Google
+      </Button>
+    </div>
+  );
 }
