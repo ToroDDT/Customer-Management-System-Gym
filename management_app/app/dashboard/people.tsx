b47324bd995e10d49f4bdcedb68a2fc589ba592c
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-02-24 18:12:50 -0800

    Add margin and padding to elements

--- management_app/app/dashboard/people.tsx
+++ management_app/app/dashboard/people.tsx
@@ -1,19 +1,33 @@
 import LeadsInfo from "../members/lead_form";
-function People() {
+export default function People() {
   return (
-    <div>
-      <div>People</div>
-      <div>
+    <div className=" bg-white mx-5 mb-5">
+      <div className="bg-blue-600 text-white p-5 rounded-md">People</div>
+      <div className="pb-2 pt-10 border-b-2 border-blue-600 mx-2">
         New Leads <span></span>
       </div>
       <LeadsInfo />
-      <div>
+      <div className="pb-2 pt-10 border-b-2 border-blue-600 mx-2">
         My Leads <span></span>
       </div>
-      <div>New Memberships</div>
-      <div></div>
-      <div>Birthdays</div>
-      <div>Anniversaries</div>
+      <div className="pb-2 pt-10 border-b-2 border-blue-600 mx-2">
+        New Memberships
+      </div>
+      <div className="flex justify-center pt-10 pb-15 mt-5 text-zinc-400">
+        New Members show up here.{" "}
+      </div>
+      <div className="pb-2 pt-10 border-b-2 border-blue-600 mx-2 pt-10 pb-15 mt-5">
+        Birthdays
+      </div>
+      <div className="flex justify-center pt-10 pb-15 mt-5 text-zinc-400">
+        This month's birthdays to celebrate.{" "}
+      </div>
+      <div className="pb-2 pt-10 border-b-2 border-blue-600 mx-2 pt-10 pb-15 mt-5">
+        Anniversaries
+      </div>
+      <div className="flex justify-center pt-10 pb-10 mt-5 text-zinc-400">
+        This months's anniversaries to acknowledge.{" "}
+      </div>
     </div>
   );
 }
