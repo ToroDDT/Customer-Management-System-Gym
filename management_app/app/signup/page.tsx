854153dcf3a729f0993a8aec6c68d17b1fc7b8bb
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-04-02 21:38:10 -0700

    Add margin and padding to input and form elements

--- management_app/app/signup/page.tsx
+++ management_app/app/signup/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Button from "@mui/material/Button";
 import { useState } from "react";
 
@@ -6,44 +7,70 @@ function Page() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
     <>
-      <form action="" method="post">
+      <form
+        className="flex flex-col mt-40 box-border pr-32 pl-10 pb-32 bg-white rounded-lg mr-10"
+        action=""
+        method="post"
+      >
+        <div className="pt-10 pb-10 text-3xl">Create Account</div>
         <label htmlFor="username">
           <input
+            className="pr-20 pt-3 pb-3 bg-slate-200 rounded-lg mb-3"
             type="text"
             placeholder="UserName"
             value={userName}
+            required
             onChange={(e) => setUserName(e.target.value)}
           />
         </label>
         <label htmlFor="email">
           <input
+            className="pr-20 pt-3 pb-3 bg-slate-200 rounded-lg mb-3"
             type="text"
             placeholder="Email"
             value={email}
+            required
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label htmlFor="password">
           <input
+            className="pr-20 pt-3 pb-3 bg-slate-200 rounded-lg mb-3"
             type="text"
             placeholder="password"
+            required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <label htmlFor="confirm-password">
           <input
+            className="pr-20 pt-3 pb-3 bg-slate-200 rounded-lg"
             type="text"
             placeholder="confirm password"
             value={confirmPassword}
+            required
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
-        <Button variant="contained">SIGN UP</Button>
-        <Button variant="contained">Sign IN</Button>
+        <div>
+          <Button
+            variant="contained"
+            type="submit"
+            className="bg-blue-800 mr-5"
+          >
+            SIGN UP
+          </Button>
+          <Button variant="contained" className="bg-blue-800">
+            Sign IN
+          </Button>
+        </div>
       </form>
     </>
   );
 }
+
+export default Page;
