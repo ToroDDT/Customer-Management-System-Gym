03e3add2ff3343c45c8a3284ba52ba95cf754e50
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-04-02 18:51:10 -0700

    Control sign up inputs with a state variable

--- management_app/app/signup/page.tsx
+++ management_app/app/signup/page.tsx
@@ -1,20 +1,45 @@
 import Button from "@mui/material/Button";
+import { useState } from "react";
 
 function Page() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   return (
     <>
       <form action="" method="post">
-        <label htmlFor="name">
-          <input type="text" placeholder="Name" />
+        <label htmlFor="username">
+          <input
+            type="text"
+            placeholder="UserName"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </label>
         <label htmlFor="email">
-          <input type="text" placeholder="Email" />
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label htmlFor="password">
-          <input type="text" placeholder="password" />
+          <input
+            type="text"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <label htmlFor="confirm-password">
-          <input type="text" placeholder="confirm password" />
+          <input
+            type="text"
+            placeholder="confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </label>
         <Button variant="contained">SIGN UP</Button>
         <Button variant="contained">Sign IN</Button>
