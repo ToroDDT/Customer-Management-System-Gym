4e8ebfab9a2bfa95cf52074f56d52094ce6f3523
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-04-15 18:20:41 -0700

    Add testing libraries

--- management_app/next.config.js
+++ management_app/next.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  env: {
-    AUTH_SECRET: "UDr2rAMw2pXRb7UVeVMIXEGBT67DnV9fS9//sT3VOVE=",
-  },
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, "bcrypt"];
+    return config;
   },
 };
