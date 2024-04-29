19203cb56bdebdee1b0ff0bc52773bf2feb851bc
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-04-28 18:19:58 -0700

    Remove body element

--- management_app/app/dashboard/layout.tsx
+++ management_app/app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import { SideNav } from "../SideNavBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <body className=" flex flex-row">
+    <div className=" flex flex-row">
       <div className="bg-blue-600">
         <SideNav />
       </div>
       <div className="mt-10">{children}</div>
-    </body>
+    </div>
   );
 }
